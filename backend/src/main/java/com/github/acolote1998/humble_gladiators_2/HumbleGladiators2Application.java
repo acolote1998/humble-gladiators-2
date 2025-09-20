5c6267b7b99dd2a01bb395705ac77cebc4796ea2
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-20 15:44:32 +0200

    initialising project

---
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/HumbleGladiators2Application.java
@@ -0,0 +1,13 @@
+package com.github.acolote1998.humble_gladiators_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumbleGladiators2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumbleGladiators2Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/acolote1998/humble_gladiators_2/HumbleGladiators2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.acolote1998.humble_gladiators_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumbleGladiators2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
