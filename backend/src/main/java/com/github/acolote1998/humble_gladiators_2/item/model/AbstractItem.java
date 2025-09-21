edf577443a8bceacf634f8217611d8a5864a379d
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-21 21:55:45 +0200

    abstract item class

---
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/item/model/AbstractItem.java
@@ -0,0 +1,27 @@
+package com.github.acolote1998.humble_gladiators_2.item.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class AbstractItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Integer rarity;
+    private Integer tier;
+    private Integer value;
+    private Integer physicalDamage;
+    private Integer magicalDamage;
+    private Boolean discovered;
+    private Integer quantity;
+    private Boolean equipped;
+}
