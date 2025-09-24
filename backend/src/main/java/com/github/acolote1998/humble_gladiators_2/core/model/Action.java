ad9e28c4b16670e960388de0f1b958275896f019
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-24 16:07:17 +0200

    fix: improve Action class annotations and field types
    - Add Lombok @Getter and @Setter annotations
    - Change primitive int fields to Integer for nullability
    - Make fields private for better encapsulation

--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/model/Action.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/model/Action.java
@@ -3,11 +3,15 @@ package com.github.acolote1998.humble_gladiators_2.core.model;
 import com.github.acolote1998.humble_gladiators_2.core.enums.ActionType;
 import com.github.acolote1998.humble_gladiators_2.core.enums.StateType;
 import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
 
 @Embeddable
+@Getter
+@Setter
 public class Action {
-    int damageCaused;
-    int healingCaused;
-    ActionType actionType;
-    StateType stateCaused;
+    private Integer damageCaused;
+    private Integer healingCaused;
+    private ActionType actionType;
+    private StateType stateCaused;
 }
