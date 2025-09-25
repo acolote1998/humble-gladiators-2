3be83b5009d950d1c3304e98daa88ea094218b7d
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-25 16:04:41 +0200

    adding basic campaign service + repository

--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/model/Theme.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/model/Theme.java
@@ -1,17 +1,28 @@
 package com.github.acolote1998.humble_gladiators_2.core.model;
 
-import jakarta.persistence.Embeddable;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 
-@Embeddable
+@Entity
 @Getter
 @Setter
 public class Theme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ElementCollection
     private List<String> wantedThemes;
+
+    @ElementCollection
     private List<String> unwantedThemes;
+
+    @OneToOne(mappedBy = "theme")
+    Campaign campaign;
 
     @Override
     public String toString() {
