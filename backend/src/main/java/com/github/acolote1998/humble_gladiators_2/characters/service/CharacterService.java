4afd3dff7b56879dbc42c7d028681345d86b146b
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-27 19:20:27 +0200

    test method for npc generation

--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/characters/service/CharacterService.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/characters/service/CharacterService.java
@@ -1,9 +1,16 @@
 package com.github.acolote1998.humble_gladiators_2.characters.service;
 
+import com.github.acolote1998.humble_gladiators_2.characters.model.CharacterInstance;
 import com.github.acolote1998.humble_gladiators_2.characters.repository.CharacterInstanceRepository;
+import com.github.acolote1998.humble_gladiators_2.core.dto.ItemFromGeminiDto;
+import com.github.acolote1998.humble_gladiators_2.core.model.Campaign;
 import com.github.acolote1998.humble_gladiators_2.core.service.GeminiService;
+import com.github.acolote1998.humble_gladiators_2.item.templates.ArmorTemplate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -14,5 +21,12 @@ public class CharacterService {
     public CharacterService(GeminiService geminiService, CharacterInstanceRepository characterInstanceRepository) {
         this.geminiService = geminiService;
         this.characterInstanceRepository = characterInstanceRepository;
+    }
+
+    public List<CharacterInstance> createFiftyNPCs(Campaign campaign) {
+        List<ItemFromGeminiDto> generatedDtos = geminiService.generateFiftyNPCs(campaign);
+
+        List<CharacterInstance> test = new ArrayList<>();
+        return test;
     }
 }
