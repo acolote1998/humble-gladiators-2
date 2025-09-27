f603ba1f7de4c381f72ceccffa34eb76d308270b
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-27 19:18:52 +0200

    adding gemini service and character instance repository to character service

--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/characters/service/CharacterService.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/characters/service/CharacterService.java
@@ -1,9 +1,18 @@
 package com.github.acolote1998.humble_gladiators_2.characters.service;
 
+import com.github.acolote1998.humble_gladiators_2.characters.repository.CharacterInstanceRepository;
+import com.github.acolote1998.humble_gladiators_2.core.service.GeminiService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class CharacterService {
+    GeminiService geminiService;
+    CharacterInstanceRepository characterInstanceRepository;
+
+    public CharacterService(GeminiService geminiService, CharacterInstanceRepository characterInstanceRepository) {
+        this.geminiService = geminiService;
+        this.characterInstanceRepository = characterInstanceRepository;
+    }
 }
