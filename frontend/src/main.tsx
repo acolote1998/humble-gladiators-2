4574efe9f070c77cd8756eabb626057c08520ad7
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-28 21:11:35 +0200

    adding clerk provider to main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import { ClerkProvider } from "@clerk/clerk-react";
+
 const queryClient = new QueryClient();
 const router = createRouter({ routeTree });
 declare module "@tanstack/react-router" {
@@ -11,10 +13,19 @@ declare module "@tanstack/react-router" {
     router: typeof router;
   }
 }
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ClerkProvider>
   </StrictMode>
 );
