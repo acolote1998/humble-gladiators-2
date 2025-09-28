eaa97d33424e77f3f0bfe372fe6fb6e9336abf31
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-28 21:40:26 +0200

    creating game controller

---
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/controller/GameController.java
@@ -0,0 +1,11 @@
+package com.github.acolote1998.humble_gladiators_2.core.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/game/")
+@CrossOrigin
+public class GameController {
+}
