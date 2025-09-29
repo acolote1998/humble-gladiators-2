9035dfa596937fb746dd39a26b9b8642b1b150a6
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-29 21:14:16 +0200

    refa createCampaign function

--- frontend/src/api/createCampaign.ts
+++ frontend/src/api/createCampaign.ts
@@ -1,4 +1,4 @@
-import axios, { HttpStatusCode } from "axios";
+import axios from "axios";
 import { BACKEND_URL } from "../util/backendUrl";
 import type { CreateCampaignType } from "../types/campaignTypes";
 
@@ -6,11 +6,16 @@ export const createCampaign = async (
   campaignToCreate: CreateCampaignType,
   bearerToken: string
 ) => {
-  const response = await axios.post(BACKEND_URL, campaignToCreate, {
-    headers: { Authorization: `Bearer ${bearerToken}` },
-  });
-  if (response.status === HttpStatusCode.Ok) {
+  try {
+    const response = await axios.post(
+      `${BACKEND_URL}/game/campaign`,
+      campaignToCreate,
+      {
+        headers: { Authorization: `Bearer ${bearerToken}` },
+      }
+    );
     return response.data;
+  } catch (error) {
+    console.log(error);
   }
-  throw new Error("Could not create campaign");
 };
