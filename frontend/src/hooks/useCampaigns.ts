cecd305271a698c1d2d16cbe476d00d55fd0c3fe
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-30 15:56:42 +0200

    creating queries for getting all campaigns, and also a campaign for a user

--- frontend/src/hooks/useCampaigns.ts
+++ frontend/src/hooks/useCampaigns.ts
@@ -3,6 +3,10 @@ import { useAuth } from "@clerk/clerk-react";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import type { CreateCampaignType } from "../types/campaignTypes";
 import { fetchCampaignCreationState } from "../api/fetchGameCreationState";
+import {
+  fetchAllChampaignsForAUser,
+  fetchCampaignByIdForAUser,
+} from "../api/fetchCampaigns";
 export const useCreateCampaign = () => {
   const { getToken } = useAuth();
   const mutation = useMutation({
@@ -27,6 +31,36 @@ export const useGetCreationCampaignState = () => {
         throw new Error("No bearer token available");
       }
       return fetchCampaignCreationState(bearerToken);
+    },
+  });
+  return { data, isError, isLoading };
+};
+
+export const useGetAllCampaignsForAUser = () => {
+  const { getToken } = useAuth();
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["campaigns"],
+    queryFn: async () => {
+      const bearerToken = await getToken();
+      if (!bearerToken) {
+        throw new Error("No bearer token available");
+      }
+      return fetchAllChampaignsForAUser(bearerToken);
+    },
+  });
+  return { data, isError, isLoading };
+};
+
+export const useGetCampaignByIdForAUser = (campaignId: number) => {
+  const { getToken } = useAuth();
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["campaign", campaignId],
+    queryFn: async () => {
+      const bearerToken = await getToken();
+      if (!bearerToken) {
+        throw new Error("No bearer token available");
+      }
+      return fetchCampaignByIdForAUser(campaignId, bearerToken);
     },
   });
   return { data, isError, isLoading };
