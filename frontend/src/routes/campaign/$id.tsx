718f4f6cba01aaf0cba5d5e73ac3d0aaa2169ecf
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-04 18:08:58 +0200

    adding fetched char data to /campaignId route

--- frontend/src/routes/campaign/$id.tsx
+++ frontend/src/routes/campaign/$id.tsx
@@ -1,27 +1,38 @@
 import { createFileRoute, useParams } from "@tanstack/react-router";
 import { useGetCampaignByIdForAUser } from "../../hooks/useCampaigns";
 import CampaignItem from "../../components/campaigns/CampaignItem";
+import { useGetCharactersByCampaignAndUser } from "../../hooks/userCharacters";
 
 export const Route = createFileRoute("/campaign/$id")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const { id } = useParams({ from: "/campaign/$id" });
+  const { id: campaignId } = useParams({ from: "/campaign/$id" });
+  const { data: characterInstancesData } = useGetCharactersByCampaignAndUser(
+    Number(campaignId)
+  );
   const {
     data: campaignData,
-    isError,
-    isLoading,
-  } = useGetCampaignByIdForAUser(Number(id));
+    isError: isCampaignError,
+    isLoading: isCampaignLoadingError,
+  } = useGetCampaignByIdForAUser(Number(campaignId));
   return (
     <>
-      {isLoading ? (
+      {isCampaignError ? (
         <p>Loading</p>
-      ) : isError ? (
+      ) : isCampaignLoadingError ? (
         <p>Error loading</p>
       ) : (
         campaignData && <CampaignItem {...campaignData} />
       )}
+      <p
+        onClick={() => {
+          console.log(characterInstancesData);
+        }}
+      >
+        Log Characters
+      </p>
     </>
   );
 }
