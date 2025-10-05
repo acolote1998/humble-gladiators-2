d510ac43fb6f7d9ce68a41b0cee89770ca2604ea
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-05 22:48:13 +0200

    adding hook to get hero from db

--- frontend/src/hooks/userCharacters.ts
+++ frontend/src/hooks/userCharacters.ts
@@ -1,6 +1,8 @@
 import { useAuth } from "@clerk/clerk-react";
 import { useQuery } from "@tanstack/react-query";
 import { fetchAllCharactersForCampaignByUser } from "../api/fetchCharacters";
+import { fetchHeroForCampaignByUser } from "../api/fetchCharacters";
+
 export const useGetCharactersByCampaignAndUser = (campaignId: number) => {
   const { getToken } = useAuth();
   const { data, isLoading, isError } = useQuery({
@@ -11,6 +13,21 @@ export const useGetCharactersByCampaignAndUser = (campaignId: number) => {
         throw new Error("No bearer token available");
       }
       return fetchAllCharactersForCampaignByUser(bearerToken, campaignId);
+    },
+  });
+  return { data, isError, isLoading };
+};
+
+export const useGetHeroByCampaignAndUser = (campaignId: number) => {
+  const { getToken } = useAuth();
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["hero-character", campaignId],
+    queryFn: async () => {
+      const bearerToken = await getToken();
+      if (!bearerToken) {
+        throw new Error("No bearer token available");
+      }
+      return fetchHeroForCampaignByUser(bearerToken, campaignId);
     },
   });
   return { data, isError, isLoading };
