ba6316c78a55125adf0b3e4fff2c5c117e74c0f3
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-05 18:34:14 +0200

    creating heroResponseDto with basic armor instance response dto

--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/characters/dto/HeroResponseDto.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/characters/dto/HeroResponseDto.java
@@ -1,4 +1,61 @@
 package com.github.acolote1998.humble_gladiators_2.characters.dto;
 
-public record HeroResponseDto() {
+import com.github.acolote1998.humble_gladiators_2.core.enums.RequirementEntryOperator;
+import com.github.acolote1998.humble_gladiators_2.core.enums.RequirementEntryType;
+import com.github.acolote1998.humble_gladiators_2.item.enums.ArmorCategory;
+
+import java.util.List;
+
+public record HeroResponseDto(
+        String name,
+        CharacterStatsResponseDto stats,
+        CharacterInventoryResponseDto inventory
+) {
+    private record CharacterStatsResponseDto(
+            int constitution,
+            int intelligence,
+            int strength,
+            int speed,
+            int luck,
+            int maxHp,
+            int currentHp,
+            int maxMp,
+            int currentMp,
+            int height,
+            int weight,
+            int level,
+            int currentExp,
+            int expForNextLevel
+    ) {
+    }
+
+    private record CharacterInventoryResponseDto(
+            List<ArmorInstanceResponseDto> armors
+    ) {
+        private record ArmorInstanceResponseDto(
+                String name,
+                String description,
+                Integer rarity,
+                Integer tier,
+                Integer value,
+                Integer quantity,
+                Boolean equipped,
+                RequirementResponseDto requirement,
+                ArmorCategory category,
+                Integer physicalDefense,
+                Integer magicalDefense
+        ) {
+
+        }
+
+        private record RequirementResponseDto(List<RequirementEntryResponseDto> requirements) {
+
+            private record RequirementEntryResponseDto(
+                    RequirementEntryType requirementType,
+                    RequirementEntryOperator operator,
+                    String value
+            ) {
+            }
+        }
+    }
 }
