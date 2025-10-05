beeed97822407cf6ffd717d2ba884c715a6f419e
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-05 11:33:46 +0200

    method getRandomItemType() in items booster service

---
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/booster/repository/ItemsBoosterRepository.java
@@ -0,0 +1,7 @@
+package com.github.acolote1998.humble_gladiators_2.booster.repository;
+
+import com.github.acolote1998.humble_gladiators_2.booster.model.ItemsBooster;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface ItemsBoosterRepository extends ListCrudRepository<Long, ItemsBooster> {
+}
--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/booster/service/ItemsBoosterService.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/booster/service/ItemsBoosterService.java
@@ -1,10 +1,18 @@
 package com.github.acolote1998.humble_gladiators_2.booster.service;
 
+import com.github.acolote1998.humble_gladiators_2.booster.enums.ItemTypesForBooster;
+import com.github.acolote1998.humble_gladiators_2.booster.model.ItemsBooster;
+import com.github.acolote1998.humble_gladiators_2.booster.repository.ItemsBoosterRepository;
 import com.github.acolote1998.humble_gladiators_2.item.service.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @Getter
@@ -19,6 +27,7 @@ public class ItemsBoosterService {
     private ShieldService shieldService;
     private SpellService spellService;
     private WeaponService weaponService;
+    private ItemsBoosterRepository itemsBoosterRepository;
 
 
     public ItemsBoosterService(ArmorService armorService,
@@ -27,7 +36,8 @@ public class ItemsBoosterService {
                                HelmetService helmetService,
                                ShieldService shieldService,
                                SpellService spellService,
-                               WeaponService weaponService) {
+                               WeaponService weaponService,
+                               ItemsBoosterRepository itemsBoosterRepository) {
         this.armorService = armorService;
         this.bootsService = bootsService;
         this.consumableService = consumableService;
@@ -35,5 +45,22 @@ public class ItemsBoosterService {
         this.shieldService = shieldService;
         this.spellService = spellService;
         this.weaponService = weaponService;
+        this.itemsBoosterRepository = itemsBoosterRepository;
+    }
+
+    ItemsBooster getItemsBooster(Long campaignId, String userId) {
+        ItemsBooster newBooster = new ItemsBooster();
+    }
+
+    private ItemTypesForBooster getRandomItemType() {
+        List<ItemTypesForBooster> itemTypes = new ArrayList<>(Arrays.asList(ItemTypesForBooster.ARMORS,
+                ItemTypesForBooster.BOOTS,
+                ItemTypesForBooster.CONSUMABLES,
+                ItemTypesForBooster.HELMETS,
+                ItemTypesForBooster.SHIELDS,
+                ItemTypesForBooster.SPELLS,
+                ItemTypesForBooster.WEAPONS));
+        Collections.shuffle(itemTypes);
+        return itemTypes.getFirst();
     }
 }
