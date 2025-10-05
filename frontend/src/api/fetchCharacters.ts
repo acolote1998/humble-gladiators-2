023114598a410231b68084cfb3325377a71f9aab
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-05 22:47:12 +0200

    api call to fetch hero from db

--- frontend/src/api/fetchCharacters.ts
+++ frontend/src/api/fetchCharacters.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { BACKEND_URL } from "../util/backendUrl";
 import type { CharacterInstanceType } from "../types/characterTypes";
+import type { HeroCharacterType } from "../types/characterTypes";
 export const fetchAllCharactersForCampaignByUser = async (
   bearerToken: string,
   campaignId: number
@@ -8,6 +9,24 @@ export const fetchAllCharactersForCampaignByUser = async (
   try {
     const response = await axios.get(
       `${BACKEND_URL}/campaign/${campaignId}/character-instances`,
+      {
+        headers: { Authorization: `Bearer ${bearerToken}` },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const fetchHeroForCampaignByUser = async (
+  bearerToken: string,
+  campaignId: number
+): Promise<HeroCharacterType> => {
+  try {
+    const response = await axios.get(
+      `${BACKEND_URL}/campaign/${campaignId}/character-instances/hero`,
       {
         headers: { Authorization: `Bearer ${bearerToken}` },
       }
