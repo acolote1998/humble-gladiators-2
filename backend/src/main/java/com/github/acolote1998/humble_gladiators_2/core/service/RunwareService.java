e99f6b1f03c1d0136bc2b91dff9f7ede0413e120
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-08 16:41:37 +0200

    creating runware service

---
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/service/RunwareService.java
@@ -0,0 +1,11 @@
+package com.github.acolote1998.humble_gladiators_2.core.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class RunwareService {
+    RestTemplate restTemplate = new RestTemplate();
+
+    
+}
