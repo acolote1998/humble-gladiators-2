159b7de99ffee5e5c1acce9086563a70c2770752
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-08 16:51:20 +0200

    getRequestBody function in runware service

--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/service/RunwareService.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/service/RunwareService.java
@@ -4,6 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
 @Service
 public class RunwareService {
     RestTemplate restTemplate = new RestTemplate();
@@ -13,4 +18,26 @@ public class RunwareService {
 
     @Value("${RUNWARE_API_KEY}")
     String API_KEY;
+
+    List<HashMap<String, Object>> getRequestBody(String prompt, String unwantedThemesContext) {
+        HashMap<String, Object> requestBodyObject = new HashMap<>();
+        requestBodyObject.put("taskType", "imageInference");
+        requestBodyObject.put("taskUUID", UUID.randomUUID().toString());
+        requestBodyObject.put("positivePrompt", prompt);
+        requestBodyObject.put("negativePrompt", unwantedThemesContext);
+        requestBodyObject.put("width", 768);
+        requestBodyObject.put("height", 576);
+        requestBodyObject.put("model", "runware:101@1");
+        requestBodyObject.put("numberResults", 1);
+        requestBodyObject.put("includeCost", true);
+
+        List<HashMap<String, Object>> requestBody = new ArrayList<>();
+        requestBody.add(requestBodyObject);
+        return requestBody;
+    }
+
+
+    String getBase64GeneratedImage() {
+
+    }
 }
