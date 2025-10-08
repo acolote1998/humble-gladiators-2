35081cc9c73adfbbed1c409fcbc24f96b3e56824
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-08 17:25:46 +0200

    removing unused import

--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/HumbleGladiators2Application.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/HumbleGladiators2Application.java
@@ -1,6 +1,5 @@
 package com.github.acolote1998.humble_gladiators_2;
 
-import com.github.acolote1998.humble_gladiators_2.core.service.RunwareService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,10 +8,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class HumbleGladiators2Application {
 
     public static void main(String[] args) throws InterruptedException {
-
-
+        
         ConfigurableApplicationContext context = SpringApplication.run(HumbleGladiators2Application.class, args);
-
-        context.getBean(RunwareService.class).getBase64GeneratedImage();
     }
 }
