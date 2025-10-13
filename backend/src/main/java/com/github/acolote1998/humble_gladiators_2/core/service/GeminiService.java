9c2e22e05edf2f6b7c6ce2f6d38d225024c47a4c
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-13 20:36:16 +0200

    moving method to generate runware armor template to gemini service

--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/service/GeminiService.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/service/GeminiService.java
@@ -33,6 +33,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import static com.github.acolote1998.humble_gladiators_2.core.util.PromptAider.*;
+
 @RequiredArgsConstructor
 @Slf4j
 @Service
@@ -617,9 +619,33 @@ public class GeminiService {
         return generatedCharacters;
     }
 
-    public String getPositivePromptForRuneware(String promptForGemini) {
+    public String getPositiveArmorPromptForRuneware(
+            Campaign campaign,
+            ArmorTemplate armorTemplate) {
         log.info("Trying to generate prompt for runeware to generate an image");
         String geminiAnswer = "";
+        String promptForGemini = String.format("""
+                        You have to generate a prompt that will be sent to an AI that will generate high-quality fantasy artwork for a trading card game.
+                        For generating the prompt, use this context:
+                        You are generating high-quality fantasy artwork for a trading card in an RPG game.
+                        - The object to illustrate is of type: %s
+                        - Focus strictly on the requested subject. Do not include any additional or implied elements unless explicitly specified \s
+                        (e.g., if illustrating an armor, render only the armor—no body, mannequin, or person wearing it unless instructed or included \s
+                        in the object name or description).
+                        - The card belongs to the campaign theme: %s.
+                        - The object to illustrate is: "%s".
+                        - A description of the object (for extra context): "%s"
+                        - Details needed: %s
+                        - Details needed: %s
+                        %s
+                        """,
+                "Armor",
+                campaign.getTheme().getWantedThemes().toString(),
+                armorTemplate.getName(),
+                armorTemplate.getDescription(),
+                TierToContext(armorTemplate.getTier()),
+                RarityToContext(armorTemplate.getRarity()),
+                GetCardImageGenerationGeneralRules());
         try {
             geminiAnswer = callGemini(promptForGemini);
             log.info("Prompt for Runeware is ready");
--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/service/RunwareService.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/service/RunwareService.java
@@ -87,29 +87,8 @@ public class RunwareService {
 
     public byte[] generateArmorTemplateImageToBytes(Campaign campaign, ArmorTemplate armorTemplate) {
         log.info(String.format("Attempt to generate image for %s - %s", armorTemplate.getName(), ArmorTemplate.class));
-        String promptForGemini = String.format("""
-                        You have to generate a prompt that will be sent to an AI that will generate high-quality fantasy artwork for a trading card game.
-                        For generating the prompt, use this context:
-                        You are generating high-quality fantasy artwork for a trading card in an RPG game.
-                        - The object to illustrate is of type: %s
-                        - Focus strictly on the requested subject. Do not include any additional or implied elements unless explicitly specified \s
-                        (e.g., if illustrating an armor, render only the armor—no body, mannequin, or person wearing it unless instructed or included \s
-                        in the object name or description).
-                        - The card belongs to the campaign theme: %s.
-                        - The object to illustrate is: "%s".
-                        - A description of the object (for extra context): "%s"
-                        - Details needed: %s
-                        - Details needed: %s
-                        %s
-                        """,
-                "Armor",
-                campaign.getTheme().getWantedThemes().toString(),
-                armorTemplate.getName(),
-                armorTemplate.getDescription(),
-                TierToContext(armorTemplate.getTier()),
-                RarityToContext(armorTemplate.getRarity()),
-                GetCardImageGenerationGeneralRules());
-        String positivePrompt = geminiService.getPositivePromptForRuneware(promptForGemini);
+
+        String positivePrompt = geminiService.getPositiveArmorPromptForRuneware(campaign, armorTemplate);
         String negativePrompt = BuildNegativePromptForRunware(campaign.getTheme().getUnwantedThemes().toString());
 
         ResponseEntity<RunwareImageGenResponse> response = sendRequestToImageGenerator(positivePrompt, negativePrompt);
