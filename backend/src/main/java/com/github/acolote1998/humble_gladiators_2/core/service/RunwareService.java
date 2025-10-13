1d0feb71c1670bc2694e89cbf651ea9f20107799
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-13 20:42:39 +0200

    moving method to generate runware spell template img to gemini service

--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/service/RunwareService.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/service/RunwareService.java
@@ -207,31 +207,8 @@ public class RunwareService {
 
     public byte[] generateSpellTemplateImageToBytes(Campaign campaign, SpellTemplate spellTemplate) {
         log.info(String.format("Attempt to generate image for %s - %s", spellTemplate.getName(), SpellTemplate.class));
-        String promptForGemini = String.format("""
-                        You have to generate a prompt that will be sent to an AI that will generate high-quality fantasy artwork for a trading card game.
-                        For generating the prompt, use this context:
-                        You are generating high-quality fantasy artwork for a trading card in an RPG game.
-                        - The object to illustrate is of type: %s
-                        - The spell category is: %s
-                        - Focus strictly on the requested subject. Do not include any additional or implied elements unless explicitly specified \s
-                        (e.g., if illustrating a spell, render the magical effect, energy, or manifestation—no background characters or additional objects unless instructed or included \s
-                        in the object name or description).
-                        - The card belongs to the campaign theme: %s.
-                        - The object to illustrate is: "%s".
-                        - A description of the object (for extra context): "%s"
-                        - Details needed: %s
-                        - Details needed: %s
-                        %s
-                        """,
-                "Spell",
-                spellTemplate.getCategory(),
-                campaign.getTheme().getWantedThemes().toString(),
-                spellTemplate.getName(),
-                spellTemplate.getDescription(),
-                TierToContext(spellTemplate.getTier()),
-                RarityToContext(spellTemplate.getRarity()),
-                GetCardImageGenerationGeneralRules());
-        String positivePrompt = geminiService.getPositivePromptForRuneware(promptForGemini);
+
+        String positivePrompt = geminiService.getPositiveSpellPromptForRuneware(campaign, spellTemplate);
         String negativePrompt = BuildNegativePromptForRunware(campaign.getTheme().getUnwantedThemes().toString());
 
         ResponseEntity<RunwareImageGenResponse> response = sendRequestToImageGenerator(positivePrompt, negativePrompt);
