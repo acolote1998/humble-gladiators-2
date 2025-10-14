0c64e31f3c9d19d934883e32b4e124d36fa848f0
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-14 15:17:12 +0200

    implemented findRandomByCampaignAndRarityAndTier in character instance repository

--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/characters/repository/CharacterInstanceRepository.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/characters/repository/CharacterInstanceRepository.java
@@ -2,7 +2,10 @@ package com.github.acolote1998.humble_gladiators_2.characters.repository;
 
 import com.github.acolote1998.humble_gladiators_2.characters.enums.CharacterType;
 import com.github.acolote1998.humble_gladiators_2.characters.model.CharacterInstance;
+import com.github.acolote1998.humble_gladiators_2.item.templates.ArmorTemplate;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,6 +15,27 @@ public interface CharacterInstanceRepository extends ListCrudRepository<Characte
     CharacterInstance findFirstByCampaign_IdAndUserIdAndCharacterType(Long campaignId, String userId, CharacterType characterType);
 
     List<CharacterInstance> findAllByCampaign_IdAndCharacterType(Long campaignId, CharacterType characterType);
-    
+
     List<CharacterInstance> findAllByTierAndCampaign_Id(Integer tier, Long campaignId);
+
+    @Query(
+            value = """
+                    SELECT at.* 
+                    FROM character_instance ci
+                    JOIN campaign c ON at.campaign_id = c.id
+                    WHERE c.id = :campaignId
+                      AND c.user_id = :userId
+                      AND ci.rarity = :rarity
+                      AND ci.tier = :tier
+                    ORDER BY RANDOM()
+                    LIMIT 1
+                    """,
+            nativeQuery = true
+    )
+    CharacterInstance findRandomByCampaignAndRarityAndTier(
+            @Param("campaignId") Long campaignId,
+            @Param("userId") String userId,
+            @Param("rarity") Integer rarity,
+            @Param("tier") Integer tier
+    );
 }
