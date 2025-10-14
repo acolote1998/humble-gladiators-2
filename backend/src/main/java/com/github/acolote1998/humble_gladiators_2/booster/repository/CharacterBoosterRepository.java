9cf0901179c8d71c5c392f5d034426e65e1ec64d
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-14 15:05:45 +0200

    creating character booster repository

---
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/booster/repository/CharacterBoosterRepository.java
@@ -0,0 +1,7 @@
+package com.github.acolote1998.humble_gladiators_2.booster.repository;
+
+import com.github.acolote1998.humble_gladiators_2.booster.model.CharacterBooster;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface CharacterBoosterRepository extends ListCrudRepository<CharacterBooster, Long> {
+}
