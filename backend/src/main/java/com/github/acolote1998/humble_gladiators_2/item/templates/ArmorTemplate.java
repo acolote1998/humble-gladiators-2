a04388199450260093bbf797c3204fad835c1bdc
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-15 14:57:29 +0200

    generating armor validation method

--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/item/templates/ArmorTemplate.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/item/templates/ArmorTemplate.java
@@ -48,4 +48,70 @@ public class ArmorTemplate extends AbstractItem {
                 //  - At least one of these flags must be 1
                 //  - Both cannot be 0.""", campaignId.toString());
     }
+
+    public static boolean isValidArmor(ArmorTemplate armor) {
+        if (armor == null) {
+            log.warn("ArmorTemplate is null");
+            return false;
+        }
+
+        if (armor.getName() == null || armor.getName().isBlank()) {
+            log.info("{} has invalid name", armor);
+            return false;
+        }
+
+        if (armor.getDescription() == null || armor.getDescription().isBlank()) {
+            log.info("{} has invalid description", armor);
+            return false;
+        }
+
+        if (armor.getRarity() == null || armor.getRarity() < 1 || armor.getRarity() > 5) {
+            log.info("{} has invalid rarity (expected 1–5)", armor);
+            return false;
+        }
+
+        if (armor.getTier() == null || armor.getTier() < 1 || armor.getTier() > 5) {
+            log.info("{} has invalid tier (expected 1-5)", armor);
+            return false;
+        }
+
+        if (armor.getValue() == null || armor.getValue() < 0) {
+            log.info("{} has invalid value", armor);
+            return false;
+        }
+
+        if (armor.getQuantity() == null || armor.getQuantity() < 0 || armor.getQuantity() > 1) {
+            log.info("{} has invalid quantity", armor);
+            return false;
+        }
+
+        if (armor.getUserId() == null || armor.getUserId().isBlank()) {
+            log.info("{} has invalid userId", armor);
+            return false;
+        }
+
+        if (armor.getCampaign() == null) {
+            log.info("{} has no campaign assigned", armor);
+            return false;
+        }
+
+        if (armor.getRequirement() == null) {
+            log.info("{} has no requirement assigned", armor);
+            return false;
+        }
+
+        if (armor.getPhysicalDefense() == null || armor.getPhysicalDefense() < 0) {
+            log.info("{} has invalid physical defense", armor);
+            return false;
+        }
+        if (armor.getMagicalDefense() == null || armor.getMagicalDefense() < 0) {
+            log.info("{} has invalid magical defense", armor);
+            return false;
+        }
+        if (armor.getPhysicalDefense() == 0 && armor.getMagicalDefense() == 0) {
+            log.info("{} has 0 in both phsyical and magical defense", armor);
+            return false;
+        }
+        return true;
+    }
 }
