9988caaed151eabf76972d5e787f53295f67a4df
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-16 10:24:29 +0200

    creating validator for requirement class

--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/model/Requirement.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/model/Requirement.java
@@ -55,69 +55,11 @@ public class Requirement {
 
     public static boolean isValidRequirement(Requirement requirement) {
         if (requirement != null) {
-            if (armor == null) {
-                log.warn("ArmorTemplate is null");
+            if (!RequirementEntry.areValidRequirementEntries(requirement.getRequirements())) {
+                log.warn("Requirement contains invalid requirementEntries");
                 return false;
             }
-
-            if (armor.getName() == null || armor.getName().isBlank()) {
-                log.warn("{} has invalid name", armor);
-                return false;
-            }
-
-            if (armor.getDescription() == null || armor.getDescription().isBlank()) {
-                log.warn("{} has invalid description", armor);
-                return false;
-            }
-
-            if (armor.getRarity() == null || armor.getRarity() < 1 || armor.getRarity() > 5) {
-                log.warn("{} has invalid rarity (expected 1–5)", armor);
-                return false;
-            }
-
-            if (armor.getTier() == null || armor.getTier() < 1 || armor.getTier() > 5) {
-                log.warn("{} has invalid tier (expected 1-5)", armor);
-                return false;
-            }
-
-            if (armor.getValue() == null || armor.getValue() < 0) {
-                log.warn("{} has invalid value", armor);
-                return false;
-            }
-
-            if (armor.getQuantity() == null || armor.getQuantity() < 0 || armor.getQuantity() > 1) {
-                log.warn("{} has invalid quantity", armor);
-                return false;
-            }
-
-            if (armor.getUserId() == null || armor.getUserId().isBlank()) {
-                log.warn("{} has invalid userId", armor);
-                return false;
-            }
-
-            if (armor.getCampaign() == null) {
-                log.warn("{} has no campaign assigned", armor);
-                return false;
-            }
-
-            if (armor.getRequirement() == null) {
-                log.warn("{} has no requirement assigned", armor);
-                return false;
-            }
-
-            if (armor.getPhysicalDefense() == null || armor.getPhysicalDefense() < 0) {
-                log.warn("{} has invalid physical defense", armor);
-                return false;
-            }
-            if (armor.getMagicalDefense() == null || armor.getMagicalDefense() < 0) {
-                log.warn("{} has invalid magical defense", armor);
-                return false;
-            }
-            if (armor.getPhysicalDefense() == 0 && armor.getMagicalDefense() == 0) {
-                log.warn("{} has 0 in both physical and magical defense", armor);
-                return false;
-            }
-            return true;
         }
+        return true;
     }
 }
