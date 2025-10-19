e253605a2851476c65c047a77addd72a4aced05c
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-19 17:35:03 +0200

    small refactor to actiontype

--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/enums/ActionType.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/enums/ActionType.java
@@ -1,5 +1,5 @@
 package com.github.acolote1998.humble_gladiators_2.core.enums;
 
 public enum ActionType {
-    HEAL, SPELL, PHYSICATTACK, RANGEDATTACK, ITEM
+    HEAL, MAGICAL_ATTACK, PHYSICAL_ATTACK, ITEM
 }
