5f1750f0f04ab77fa29759612676ca7ca1a8c9ac
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-20 10:32:40 +0200

    method for creating a new battle

--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/service/BattleService.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/service/BattleService.java
@@ -1,21 +1,25 @@
 package com.github.acolote1998.humble_gladiators_2.core.service;
 
 import com.github.acolote1998.humble_gladiators_2.characters.exception.DailyEnemyNotFound;
-import com.github.acolote1998.humble_gladiators_2.characters.exception.HeroDoesNotExist;
 import com.github.acolote1998.humble_gladiators_2.characters.model.CharacterInstance;
 import com.github.acolote1998.humble_gladiators_2.characters.service.CharacterService;
 import com.github.acolote1998.humble_gladiators_2.core.model.Battle;
+import com.github.acolote1998.humble_gladiators_2.core.model.Turn;
 import com.github.acolote1998.humble_gladiators_2.core.repository.BattleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Service
 @Slf4j
 public class BattleService {
     CharacterService characterService;
+
     BattleRepository battleRepository;
 
     @Autowired
@@ -26,9 +30,52 @@ public class BattleService {
         this.battleRepository = battleRepository;
     }
 
-    public Battle createNewBattle(Long campaignId, String userId, Long enemyId) {
+    public Battle createNewBattle(Long campaignId, String userId) {
         CharacterInstance hero = characterService.getHero(campaignId, userId);
+        CharacterInstance enemy = characterService.getDailyEnemy(campaignId, userId);
+        List<Turn> emptyTurnList = new ArrayList<>();
+        List<CharacterInstance> teamOne = new ArrayList<>();
+        teamOne.add(hero);
+        List<CharacterInstance> teamTwo = new ArrayList<>();
+        teamTwo.add(enemy);
+        List<CharacterInstance> winningTeam = new ArrayList<>();
+        List<CharacterInstance> losingTeam = new ArrayList<>();
+        CharacterInstance startingCharacter = whoStartsTheBattle(teamOne, teamTwo);
+        Battle newBattle = new Battle();
+        newBattle.setCampaign(hero.getCampaign());
+        newBattle.setUserId(userId);
+        newBattle.setTurns(emptyTurnList);
+        newBattle.setWinningTeam(winningTeam);
+        newBattle.setLosingTeam(losingTeam);
+        newBattle.setTeamOne(teamOne);
+        newBattle.setTeamTwo(teamTwo);
+        newBattle.setCurrentCharacterToPlay(startingCharacter);
+        newBattle.setOngoing(true);
+        return newBattle;
     }
+
+    private CharacterInstance whoStartsTheBattle(List<CharacterInstance> teamOne, List<CharacterInstance> teamTwo) {
+        List<CharacterInstance> allChars = new ArrayList<>();
+        allChars.addAll(teamOne);
+        allChars.addAll(teamTwo);
+
+        Random random = new Random();
+        CharacterInstance fastest = allChars.getFirst();
+
+        for (CharacterInstance character : allChars) {
+            int speed = character.getStats().getSpeed();
+            int currentSpeed = fastest.getStats().getSpeed();
+
+            if (speed > currentSpeed) {
+                fastest = character;
+            } else if (speed == currentSpeed && random.nextInt(1, 3) == 1) {
+                fastest = character;
+            }
+        }
+
+        return fastest;
+    }
+
 
     public boolean isBattleAvailableForToday(Long campaignId, String userId) {
         if (!characterService.doesHeroExistForACampaign(campaignId, userId)) {
