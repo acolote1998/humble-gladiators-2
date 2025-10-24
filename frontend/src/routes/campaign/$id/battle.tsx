5a3503a1093433994ba5fd2474331c2ad67e13c0
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-24 16:35:00 +0200

    rendering battle outcomes properly on battle route

--- frontend/src/routes/campaign/$id/battle.tsx
+++ frontend/src/routes/campaign/$id/battle.tsx
@@ -1,6 +1,7 @@
 import {
   useGetBattleCreationAvailability,
   useCreateABattleForTodayByCampaignIdAndUser,
+  useGetBattleForTodayByCampaignIdAndUsery,
 } from "../../../hooks/useBattles";
 import { useParams } from "@tanstack/react-router";
 import { createFileRoute } from "@tanstack/react-router";
@@ -11,18 +12,38 @@ export const Route = createFileRoute("/campaign/$id/battle")({
 
 function RouteComponent() {
   const { id: campaignId } = useParams({ from: "/campaign/$id/battle" });
-  const { data: createdBattle, mutate: createBattle } =
+  const { mutate: createBattle } =
     useCreateABattleForTodayByCampaignIdAndUser();
   const {
     data: isBattleCreationPossible,
     isError: isBattleCreationPossibleError,
     isLoading: isBattleCreationPossibleLoading,
   } = useGetBattleCreationAvailability(Number(campaignId));
+  const {
+    data: activeBattleData,
+    isLoading: isActiveBattleLoading,
+    isError: isActiveBattleError,
+  } = useGetBattleForTodayByCampaignIdAndUsery(Number(campaignId));
   return (
     <div>
-      {!createdBattle ? (
-        isBattleCreationPossibleLoading ? (
-          <p>Loading...</p>
+      {isActiveBattleLoading ? (
+        <p>Loading battle...</p>
+      ) : isActiveBattleError ? (
+        <p>Error loading battle...</p>
+      ) : (
+        activeBattleData && (
+          <p
+            onClick={() => {
+              console.log(activeBattleData);
+            }}
+          >
+            Check the active battle
+          </p>
+        )
+      )}
+      {!activeBattleData &&
+        (isBattleCreationPossibleLoading ? (
+          <p>Loading battle creation availability...</p>
         ) : isBattleCreationPossibleError ? (
           <p>Error checking battle creation availability</p>
         ) : isBattleCreationPossible ? (
@@ -38,16 +59,7 @@ function RouteComponent() {
             Not possible to create battle, try opening a booster character or
             come back tomorrow
           </p>
-        )
-      ) : (
-        <p
-          onClick={() => {
-            console.log(createdBattle);
-          }}
-        >
-          Check the created battle
-        </p>
-      )}
+        ))}
     </div>
   );
 }
