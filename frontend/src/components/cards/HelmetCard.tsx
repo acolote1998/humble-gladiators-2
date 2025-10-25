27b5eb485f3e507e7dd47f0c145aba51d4465a29
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-25 13:38:18 +0200

    Add title descriptors to HelmetCard component

--- frontend/src/components/cards/HelmetCard.tsx
+++ frontend/src/components/cards/HelmetCard.tsx
@@ -62,10 +62,10 @@ export const HelmetCard = ({
           <div className="absolute bottom-51.5 left-0 text-sm">
             <div className="relative h-6">
               {/* container for absolute children; give it a height so top:0 has meaning */}
-              <span className="absolute left-12 top-0 z-20 w-50">
+              <span title="TIER" className="absolute left-12 top-0 z-20 w-50">
                 T {calculateTierAndRarityStars(tier)}
               </span>
-              <span className="absolute left-46 top-0 z-10 w-50">
+              <span title="RARITY" className="absolute left-46 top-0 z-10 w-50">
                 R {calculateTierAndRarityStars(rarity)}
               </span>
             </div>
@@ -88,10 +88,13 @@ export const HelmetCard = ({
                 className="w-65.5 h-auto"
               />
             )}
-            <p className="text-lg mt-8 ">
+            <p title="NAME" className="text-lg mt-8 ">
               {discovered || renderingFrom == "BOOSTER" ? name : "?"}
             </p>
-            <p className="text-sm opacity-80 text-center p-1 mt-0.5 px-7">
+            <p
+              title="DESCRIPTION"
+              className="text-sm opacity-80 text-center p-1 mt-0.5 px-7"
+            >
               {discovered || renderingFrom == "BOOSTER" ? description : "?"}
             </p>
           </div>
@@ -99,7 +102,7 @@ export const HelmetCard = ({
           {/* Bottom stats */}
           <div className="grid grid-cols-5 absolute bottom-14 text-sm">
             <div className="absolute left-7 w-15">
-              <p>
+              <p title="PHYSICAL DEFENSE">
                 🛡️{" "}
                 {discovered || renderingFrom == "BOOSTER"
                   ? physicalDefense
@@ -110,7 +113,7 @@ export const HelmetCard = ({
           <p>❤️ {(discovered || renderingFrom=="BOOSTER") ? restoreHp : "?"}</p>
         </div> */}
             <div className="absolute left-41.5 w-15">
-              <p>
+              <p title="MAGICAL DEFENSE">
                 ✨{" "}
                 {discovered || renderingFrom == "BOOSTER"
                   ? magicalDefense
