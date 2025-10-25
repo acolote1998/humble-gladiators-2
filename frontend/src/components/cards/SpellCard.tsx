042d2b435767f2b5f8d68d3e4bd72e263dbc4715
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-25 13:41:14 +0200

    Add title descriptors to SpellCard component

--- frontend/src/components/cards/SpellCard.tsx
+++ frontend/src/components/cards/SpellCard.tsx
@@ -54,10 +54,10 @@ export const SpellCard = ({
       <div className="absolute bottom-51.5 left-0 text-sm">
         <div className="relative h-6">
           {/* container for absolute children; give it a height so top:0 has meaning */}
-          <span className="absolute left-12 top-0 z-20 w-50">
+          <span title="TIER" className="absolute left-12 top-0 z-20 w-50">
             T {calculateTierAndRarityStars(tier)}
           </span>
-          <span className="absolute left-46 top-0 z-10 w-50">
+          <span title="RARITY" className="absolute left-46 top-0 z-10 w-50">
             R {calculateTierAndRarityStars(rarity)}
           </span>
         </div>
@@ -80,10 +80,13 @@ export const SpellCard = ({
             className="w-65.5 h-auto"
           />
         )}
-        <p className="text-lg mt-8 ">
+        <p title="NAME" className="text-lg mt-8 ">
           {discovered || renderingFrom == "BOOSTER" ? name : "?"}
         </p>
-        <p className="text-sm opacity-80 text-center p-1 mt-0.5 px-7">
+        <p
+          title="DESCRIPTION"
+          className="text-sm opacity-80 text-center p-1 mt-0.5 px-7"
+        >
           {discovered || renderingFrom == "BOOSTER" ? description : "?"}
         </p>
       </div>
@@ -91,15 +94,17 @@ export const SpellCard = ({
       {/* Bottom stats */}
       <div className="grid grid-cols-5 absolute bottom-14 text-sm">
         <div className="absolute left-7 w-15">
-          <p>
+          <p title="PHYSICAL DAMAGE">
             ⚔️ {discovered || renderingFrom == "BOOSTER" ? physicalDamage : "?"}
           </p>
         </div>
         <div className="absolute left-24 w-15">
-          <p>❤️ {discovered || renderingFrom == "BOOSTER" ? restoreHp : "?"}</p>
+          <p title="RESTORE HP">
+            ❤️ {discovered || renderingFrom == "BOOSTER" ? restoreHp : "?"}
+          </p>
         </div>
         <div className="absolute left-41.5 w-15">
-          <p>
+          <p title="MAGICAL DAMAGE">
             ✨ {discovered || renderingFrom == "BOOSTER" ? magicalDamage : "?"}
           </p>
         </div>
