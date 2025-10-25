441f27a8542fe6d7633712d2034a5b4da79a200f
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-25 13:08:44 +0200

    rendering hand of the hero

--- frontend/src/components/battle/BattleExecuting.tsx
+++ frontend/src/components/battle/BattleExecuting.tsx
@@ -1,5 +1,10 @@
 import type { BattleResponseDto } from "../../types/battleTypes";
+import { ArmorCard } from "../cards/ArmorCard";
+import { BootsCard } from "../cards/BootsCard";
 import { CharacterCard } from "../cards/CharacterCard";
+import { ConsumableCard } from "../cards/ConsumableCard";
+import { SpellCard } from "../cards/SpellCard";
+import { WeaponCard } from "../cards/WeaponCard";
 const BattleExecuting = ({
   campaignId,
   currentCharacterToPlay,
@@ -18,6 +23,22 @@ const BattleExecuting = ({
       <div className="flex flex-col items-center">
         <p className="text-2xl">Enemy</p>
         <CharacterCard {...teamTwo[0]} renderingFrom="BATTLE" />
+      </div>
+      <div>
+        <div className="flex flex-col items-center">
+          <p className="text-2xl">Hand</p>
+          {teamOne[0].inventory.weapons.map((card) => {
+            if (card.equipped) {
+              return <WeaponCard {...card} renderingFrom="BATTLE" />;
+            }
+          })}
+          {teamOne[0].inventory.spells.map((card) => {
+            return <SpellCard {...card} renderingFrom="BATTLE" />;
+          })}
+          {teamOne[0].inventory.consumables.map((card) => {
+            return <ConsumableCard {...card} renderingFrom="BATTLE" />;
+          })}
+        </div>
       </div>
     </div>
   );
