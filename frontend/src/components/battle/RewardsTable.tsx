6eb87c0a4529191b3b768762e92f8df116392722
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-28 20:21:04 +0100

    styling rewards table component

--- frontend/src/components/battle/RewardsTable.tsx
+++ frontend/src/components/battle/RewardsTable.tsx
@@ -12,26 +12,38 @@ const RewardsTable = ({
   characterTwoName,
 }: RewardsTableType) => {
   const navigate = useNavigate();
+  const heroHasWon = () => {
+    return rewardsForBattle.battleResult === "VICTORY_TEAM_ONE";
+  };
   const { id: campaignId } = useParams({ from: "/campaign/$id/battle" });
   return (
-    <div className="absolute rounded-md bg-gray-200 border-gray-400 border mx-5 top-20 px-5 left-0 w-150 h-110 overflow-y-scroll">
+    <div className="absolute rounded-md bg-gray-200 border-gray-400 border mx-5 top-20 px-5 left-0 w-150 h-110 overflow-y-auto">
       <p className="my-2 p-1 rounded-md bg-gray-500 text-xl text-center text-white">
         Battle Finished
       </p>
-      <p className="my-2 p-1 rounded-md bg-gray-300">
-        Winner:{" "}
+      <p
+        className={`my-2 p-1 rounded-md ${heroHasWon() ? "bg-green-400" : "bg-red-400"}  text-xl text-center text-white`}
+      >
+        {heroHasWon() ? "Victory" : "Defeat"}
+      </p>
+      <p className="my-2 p-1 rounded-md bg-gray-300 text-center">
         {rewardsForBattle?.battleResult == "VICTORY_TEAM_ONE"
           ? characterOneName
           : rewardsForBattle.battleResult == "VICTORY_TEAM_TWO" &&
-            characterTwoName}
+            characterTwoName}{" "}
+        won the battle!
       </p>
-      <p className="my-2 p-1 rounded-md bg-gray-300">
-        Exp Reward: {rewardsForBattle?.expReward}
-      </p>
-      <p className="my-2 p-1 rounded-md bg-gray-300">
-        Gold Reward: {rewardsForBattle?.goldReward}
-      </p>
-      <p className="my-2 p-1 rounded-md bg-gray-300">Item loot:</p>
+      {heroHasWon() && (
+        <>
+          <p className="my-2 p-1 rounded-md bg-gray-300">
+            Exp Reward: {rewardsForBattle?.expReward}
+          </p>
+          <p className="my-2 p-1 rounded-md bg-gray-300">
+            Gold Reward: {rewardsForBattle?.goldReward}
+          </p>
+          <p className="my-2 p-1 rounded-md bg-gray-300">Item loot:</p>
+        </>
+      )}
       {rewardsForBattle.armorLoot.length > 0 &&
         rewardsForBattle.armorLoot.map((i) => (
           <p className="my-2 p-1 rounded-md bg-gray-300">
@@ -74,14 +86,16 @@ const RewardsTable = ({
             {i.name} - T {i.tier} - R {i.rarity}
           </p>
         ))}
-      <p
-        className="my-2 p-1 rounded-md bg-red-400 text-center"
-        onClick={() => {
-          navigate({ to: `/campaign/${campaignId}` });
-        }}
-      >
-        Close Battle
-      </p>
+      <div className="flex justify-center cursor-pointer">
+        <p
+          className="my-2 p-1 rounded-md bg-yellow-200 border-yellow-500 border text-center w-fit"
+          onClick={() => {
+            navigate({ to: `/campaign/${campaignId}` });
+          }}
+        >
+          Close Battle
+        </p>
+      </div>
     </div>
   );
 };
