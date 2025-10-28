e7e39897f468d8630b5d92073d9d186bb5e148e2
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-28 12:06:43 +0100

    removing campaing info from campaign/ route

--- frontend/src/routes/campaign/$id/index.tsx
+++ frontend/src/routes/campaign/$id/index.tsx
@@ -3,8 +3,6 @@ import {
   useNavigate,
   useParams,
 } from "@tanstack/react-router";
-import { useGetCampaignByIdForAUser } from "../../../hooks/useCampaigns";
-import CampaignInfo from "../../../components/campaigns/CampaignInfo";
 import { useGetHeroExistence } from "../../../hooks/useCharacters";
 import { CharacterBooster } from "../../../components/boosters/CharacterBooster";
 import { ItemsBooster } from "../../../components/boosters/ItemsBooster";
@@ -22,90 +20,68 @@ function RouteComponent() {
     isError: doesHeroExistError,
   } = useGetHeroExistence(Number(campaignId));
 
-  const {
-    data: campaignData,
-    isError: isCampaignError,
-    isLoading: isCampaignLoading,
-  } = useGetCampaignByIdForAUser(Number(campaignId));
-
   const { data: isBattleOngoing, isLoading: isBattleOngoingLoading } =
     useGetIsBattleOngoing(Number(campaignId));
 
   return (
     <>
-      {isCampaignLoading ? (
-        <p>Loading campaign</p>
-      ) : isCampaignError ? (
-        <p>Error loading campaign</p>
-      ) : (
-        campaignData && (
+      <>
+        {doesHeroExistLoading ? (
+          <p>Loading hero availability</p>
+        ) : doesHeroExistError ? (
+          <p>Error loading hero availability</p>
+        ) : doesHeroExist ? (
           <>
-            <CampaignInfo {...campaignData} />
-            {doesHeroExistLoading ? (
-              <p>Loading hero availability</p>
-            ) : doesHeroExistError ? (
-              <p>Error loading hero availability</p>
-            ) : doesHeroExist ? (
+            {isBattleOngoingLoading ? (
+              <p>Loading...</p>
+            ) : !isBattleOngoing ? (
               <>
-                {isBattleOngoingLoading ? (
-                  <p>Loading...</p>
-                ) : !isBattleOngoing ? (
-                  <>
-                    <p
-                      onClick={() => {
-                        navigate({ to: `/campaign/${campaignId}/compendium` });
-                      }}
-                      className="bg-gray-400 p-3 rounded-lg"
-                    >
-                      Go to the compendium
-                    </p>
-                    <ItemsBooster campaignId={campaignId} />
-
-                    <CharacterBooster campaignId={campaignId} />
-
-                    <p
-                      onClick={() => {
-                        navigate({ to: `/campaign/${campaignId}/inventory` });
-                      }}
-                      className="bg-gray-400 p-3 rounded-lg"
-                    >
-                      Hero Inventory
-                    </p>
-                  </>
-                ) : (
-                  <p className="bg-red-400 p-3 rounded-lg">
-                    You cannot perform actions during an ongoing battle!!!
-                  </p>
-                )}
                 <p
                   onClick={() => {
-                    navigate({ to: `/campaign/${campaignId}/battle` });
+                    navigate({ to: `/campaign/${campaignId}/compendium` });
                   }}
                   className="bg-gray-400 p-3 rounded-lg"
                 >
-                  Battles
+                  Go to the compendium
+                </p>
+                <ItemsBooster campaignId={campaignId} />
+
+                <CharacterBooster campaignId={campaignId} />
+
+                <p
+                  onClick={() => {
+                    navigate({ to: `/campaign/${campaignId}/inventory` });
+                  }}
+                  className="bg-gray-400 p-3 rounded-lg"
+                >
+                  Hero Inventory
                 </p>
               </>
             ) : (
-              <p
-                className="p-2 bg-green-300 w-fit rounded-md m-2 border border-green-500"
-                onClick={() => {
-                  navigate({ to: `/campaign/${campaignId}/createHero` });
-                }}
-              >
-                Click here to create your hero
+              <p className="bg-red-400 p-3 rounded-lg">
+                You cannot perform actions during an ongoing battle!!!
               </p>
             )}
+            <p
+              onClick={() => {
+                navigate({ to: `/campaign/${campaignId}/battle` });
+              }}
+              className="bg-gray-400 p-3 rounded-lg"
+            >
+              Battles
+            </p>
           </>
-        )
-      )}
-      {campaignData?.coverImgBase64 && (
-        <img
-          draggable={false}
-          src={`data:image/jpeg;base64,${campaignData?.coverImgBase64}`}
-          alt={campaignData?.name}
-        />
-      )}
+        ) : (
+          <p
+            className="p-2 bg-green-300 w-fit rounded-md m-2 border border-green-500"
+            onClick={() => {
+              navigate({ to: `/campaign/${campaignId}/createHero` });
+            }}
+          >
+            Click here to create your hero
+          </p>
+        )}
+      </>
     </>
   );
 }
