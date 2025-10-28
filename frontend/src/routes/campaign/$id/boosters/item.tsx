6713ff9adfb44de02390b1b802e1f0ebfa343322
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-28 13:31:02 +0100

    rendering item booster in route

--- frontend/src/routes/campaign/$id/boosters/item.tsx
+++ frontend/src/routes/campaign/$id/boosters/item.tsx
@@ -1,9 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router";
-
+import { useParams } from "@tanstack/react-router";
+import { ItemsBooster } from "../../../../components/boosters/ItemsBooster";
 export const Route = createFileRoute("/campaign/$id/boosters/item")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/campaign/$id/boosters/item"!</div>;
+  const { id: campaignId } = useParams({
+    from: "/campaign/$id/boosters/item",
+  });
+
+  return (
+    <div>
+      <ItemsBooster campaignId={campaignId} />
+    </div>
+  );
 }
