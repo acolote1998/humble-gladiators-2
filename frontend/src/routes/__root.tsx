0feb5ae826464bdc5b79b0dd0369b803fc0556a5
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-28 12:10:04 +0100

    making humble gladiator 2 a header

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -9,6 +9,24 @@ import GetInfinityToken from "../components/GetInfinityToken";
 
 const RootLayout = () => (
   <>
+    <div className="flex items-center justify-center">
+      <h1
+        className="
+          text-6xl
+          tracking-tighter
+          font-black
+          text-center
+          p-6
+          rounded-b-2xl
+          border-l-5
+          border-r-5
+          border-b-5
+        border-gray-400 
+        bg-gray-200"
+      >
+        Humble Gladiators 2
+      </h1>
+    </div>
     <div className="absolute top-0 right-0 bg-gray-200 rounded-bl-lg border-gray-400 border p-2 flex gap-2">
       <Link to="/campaign">Campaigns</Link>
       <SignedIn>
--- frontend/src/routes/campaign/index.tsx
+++ frontend/src/routes/campaign/index.tsx
@@ -16,24 +16,6 @@ function CampaignsRoute() {
   } = useGetAllCampaignsForAUser();
   return (
     <>
-      <div className="flex items-center justify-center">
-        <h1
-          className="
-          text-6xl
-          tracking-tighter
-          font-black
-          text-center
-          p-6
-          rounded-b-2xl
-          border-l-5
-          border-r-5
-          border-b-5
-        border-gray-400 
-        bg-gray-200"
-        >
-          Humble Gladiators 2
-        </h1>
-      </div>
       <SignedIn>
         <button
           className="
