0604fdbdd9fe5d986b9caaf6f541695f8cfc05e8
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-28 13:27:23 +0100

    removing unnecessary availability checks in character booster

--- frontend/src/routes/campaign/$id/boosters/character.tsx
+++ frontend/src/routes/campaign/$id/boosters/character.tsx
@@ -11,24 +11,10 @@ function RouteComponent() {
   const { id: campaignId } = useParams({
     from: "/campaign/$id/boosters/character",
   });
-  const {
-    data: doesHeroExist,
-    isLoading: doesHeroExistLoading,
-    isError: doesHeroExistError,
-  } = useGetHeroExistence(Number(campaignId));
-  const { data: isBattleOngoing, isLoading: isBattleOngoingLoading } =
-    useGetIsBattleOngoing(Number(campaignId));
+
   return (
     <div>
-      {doesHeroExistLoading || isBattleOngoingLoading ? (
-        <p className="loader" />
-      ) : doesHeroExistError ? (
-        <p>Error...</p>
-      ) : doesHeroExist && !isBattleOngoing ? (
-        <CharacterBooster campaignId={campaignId} />
-      ) : (
-        <p>You cannot open a booster during an ongoing battle</p>
-      )}
+      <CharacterBooster campaignId={campaignId} />
     </div>
   );
 }
