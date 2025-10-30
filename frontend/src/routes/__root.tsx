18eb3c04ab839c1f20f254b9f902258da2faf244
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-30 10:51:25 +0100

    removing unused code

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -17,25 +17,22 @@ import Logo from "../components/Logo";
 
 const RootLayout = () => {
   const { location } = useRouterState();
-  const matchRoute = useMatchRoute();
   return (
     <>
-      {<Logo />}
+      <Logo />
       {location.pathname !== "/campaign" && location.pathname !== "/" && (
         <NavBar />
       )}
-      {
-        <div className="absolute top-0 right-0 bg-gray-200 rounded-bl-lg border-gray-400 border p-2 flex gap-2">
-          <Link to="/campaign">Campaigns</Link>
-          <SignedIn>
-            {/* <GetInfinityToken /> */}
-            <SignOutButton />
-          </SignedIn>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-        </div>
-      }
+      <div className="absolute top-0 right-0 bg-gray-200 rounded-bl-lg border-gray-400 border p-2 flex gap-2">
+        <Link to="/campaign">Campaigns</Link>
+        <SignedIn>
+          {/* <GetInfinityToken /> */}
+          <SignOutButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+      </div>
       <Outlet />
     </>
   );
