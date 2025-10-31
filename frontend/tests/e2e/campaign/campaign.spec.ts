b097f4b1fcc7c1ef87910eb3521436f5c99a68ba
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-10-31 09:41:25 +0100

    adding test ids to navbar components

--- frontend/tests/e2e/campaign/campaign.spec.ts
+++ frontend/tests/e2e/campaign/campaign.spec.ts
@@ -32,3 +32,27 @@ test("navigating to campaigns shows the test campaign", async ({ page }) => {
   const testCampaign = page.getByTestId(/test-Medieval Adventure/i);
   await expect(testCampaign).toBeVisible();
 });
+
+test("opening item booster and verifying the cards are in inventory", async ({
+  page,
+}) => {
+  await page.goto(FRONTEND_URL);
+  await page.getByText(/campaigns/i).click();
+  const testCampaign = page.getByTestId(/test-Medieval Adventure/i);
+  await expect(testCampaign).toBeVisible();
+  await testCampaign.click();
+  await page.getByText(/forge your hero/i).click();
+  await page.getByTestId("hero-name-input").fill("Aki Test!");
+  await page.getByText(/forge your hero/i).click();
+  await expect(page.getByText(/campaign stats/i)).toBeVisible();
+  await expect(page.getByText(/armors/i)).toBeVisible();
+  await expect(page.getByText(/boots/i)).toBeVisible();
+  await expect(page.getByText(/consumables/i)).toBeVisible();
+  await expect(page.getByText(/helmets/i)).toBeVisible();
+  await expect(page.getByText(/shields/i)).toBeVisible();
+  await expect(page.getByText(/weapons/i)).toBeVisible();
+  await expect(page.getByText(/spells/i)).toBeVisible();
+  await expect(page.getByText(/characters/i)).toBeVisible();
+  await expect(page.getByText(/win rate/i)).toBeVisible();
+  await expect(page.getByText(/forge your hero/i)).toBeHidden();
+});
