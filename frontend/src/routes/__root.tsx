801fc52435b61a1adea51c906e2795a8193410d9
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-11-01 21:20:12 +0100

    removing nav bar from campaign creation route

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -19,9 +19,9 @@ const RootLayout = () => {
   return (
     <>
       <Logo />
-      {location.pathname !== "/campaign" && location.pathname !== "/" && (
-        <NavBar />
-      )}
+      {location.pathname !== "/campaign" &&
+        location.pathname !== "/campaign/create" &&
+        location.pathname !== "/" && <NavBar />}
       <div className="absolute top-0 right-0 bg-gray-200 rounded-bl-lg border-gray-400 border p-2 flex gap-2">
         <Link to="/campaign">Campaigns</Link>
         <SignedIn>
