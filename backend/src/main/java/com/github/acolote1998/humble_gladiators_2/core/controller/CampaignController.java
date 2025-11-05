1a0bbc5154271184d06f78366e14c3a373846f95
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-11-05 23:03:32 +0100

    Fix CampaignController to return 404 when campaign not found instead of 500

--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/controller/CampaignController.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/controller/CampaignController.java
@@ -7,6 +7,7 @@ import com.github.acolote1998.humble_gladiators_2.core.service.CampaignService;
 import com.github.acolote1998.humble_gladiators_2.core.util.BytesToBase64;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -41,6 +42,9 @@ public class CampaignController {
     public ResponseEntity<CampaignResponseDto> getCampaignByUserAndId(@AuthenticationPrincipal Jwt jwt, @PathVariable Long campaignId) {
         String userId = jwt.getSubject();
         Campaign campaign = campaignService.getCampaignByIdAndUserId(userId, campaignId);
+        if (campaign == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
         return ResponseEntity.ok(CampaignResponseDto.fromEntityToCampaignResponseDto(campaign));
     }
 
