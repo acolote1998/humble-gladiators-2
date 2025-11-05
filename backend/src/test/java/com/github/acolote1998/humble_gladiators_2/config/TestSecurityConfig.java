9744d745239fe849685e2018d8d168fcfaa4d74b
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-11-06 00:10:09 +0100

    Remove unused imports from TestSecurityConfig

--- backend/src/test/java/com/github/acolote1998/humble_gladiators_2/config/TestSecurityConfig.java
+++ backend/src/test/java/com/github/acolote1998/humble_gladiators_2/config/TestSecurityConfig.java
@@ -1,9 +1,7 @@
 package com.github.acolote1998.humble_gladiators_2.config;
 
-import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
