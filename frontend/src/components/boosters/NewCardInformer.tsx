9c350ee6f25623aa25daca5228f687ad5632b606
acolote1998 <manuelacevedo.ar@gmail.com>
2025-11-08 16:42:42 +0100

    restyling NEW card button

--- frontend/src/components/boosters/NewCardInformer.tsx
+++ frontend/src/components/boosters/NewCardInformer.tsx
@@ -1,3 +1,7 @@
 export const NewCardInformer = () => {
-  return <p className="select-none bg-[var(--highlight-color)]">NEW!</p>;
+  return (
+    <span className="select-none absolute bottom-20 left-1/2 -translate-x-1/2 px-3 py-1 text-xs font-bold text-[var(--light-text)] bg-[var(--action-positive-bg)] rounded-full shadow-md animate-pulse">
+      NEW!
+    </span>
+  );
 };
