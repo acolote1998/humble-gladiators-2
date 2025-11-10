1c9837364c3c85695727b5f81fa0c4cfd9cb6bc2
acolote1998 <manuelacevedo.ar@gmail.com>
2025-11-10 23:08:06 +0100

    adjusting item and char booster bg div

--- frontend/src/routes/campaign/$id/boosters/character.tsx
+++ frontend/src/routes/campaign/$id/boosters/character.tsx
@@ -12,7 +12,7 @@ function RouteComponent() {
   });
 
   return (
-    <PageContainer h="166">
+    <PageContainer minH="76">
       <CharacterBooster campaignId={Number(campaignId)} />
     </PageContainer>
   );
