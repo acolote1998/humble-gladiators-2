8428c9cf04e64cc9403b837e0247398b3baa4099
acolote1998 <manuelacevedo.ar@gmail.com>
2025-11-13 20:40:28 +0100

    adjusting 'new' label positioning

--- frontend/src/components/boosters/NewCardInformer.tsx
+++ frontend/src/components/boosters/NewCardInformer.tsx
@@ -1,6 +1,6 @@
 export const NewCardInformer = () => {
   return (
-    <span className="select-none absolute bottom-20 left-1/2 -translate-x-1/2 px-3 py-1 text-xs font-bold text-[var(--light-text)] bg-[var(--action-positive-bg)] rounded-full shadow-md animate-pulse">
+    <span className="select-none absolute bottom-20 md:top-25 md:bottom-auto lg:top-8 xl:top-15 2xl:top-18 left-1/2 -translate-x-1/2 px-3 py-1 text-xs font-bold text-[var(--light-text)] bg-[var(--action-positive-bg)] rounded-full shadow-md animate-pulse">
       NEW!
     </span>
   );
