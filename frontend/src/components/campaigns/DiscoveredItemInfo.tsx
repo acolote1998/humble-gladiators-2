414c8834307b5a91236a2b45ce55865579320a4a
acolote1998 <manuelacevedo.ar@gmail.com>
2025-11-16 18:38:14 +0100

    making campaign stats have other pointer

--- frontend/src/components/campaigns/DiscoveredItemInfo.tsx
+++ frontend/src/components/campaigns/DiscoveredItemInfo.tsx
@@ -15,7 +15,7 @@ export const DiscoveredItemInfo = ({
       <p className="col-span-4 lg:col-span-2 text-md lg:text-lg text-center lg:font-semibold">
         {itemName}
       </p>
-      <div className="cursor-pointer col-span-6 lg:col-span-8 relative bg-[var(--highlight-color)] border-[var(--highlight-color-border)] border rounded-md h-5 lg:h-10 group">
+      <div className="col-span-6 lg:col-span-8 relative bg-[var(--highlight-color)] border-[var(--highlight-color-border)] border rounded-md h-5 lg:h-10 group">
         <div
           className={`transition-all duration-500 rounded-md border-[var(--information-color-border)] border bg-[var(--information-color)] top-0 left-0 h-full flex items-center justify-center`}
           style={{ width: `${percentDiscovered}%` }}
