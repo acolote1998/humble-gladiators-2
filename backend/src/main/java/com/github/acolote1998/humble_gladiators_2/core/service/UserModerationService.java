cd96f907b0dd362622db0c077627f4e405b938d8
acolote1998 <manuelacevedo.ar@gmail.com>
2025-11-21 10:02:58 +0100

    making user moderation service have a gemini service

--- backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/service/UserModerationService.java
+++ backend/src/main/java/com/github/acolote1998/humble_gladiators_2/core/service/UserModerationService.java
@@ -1,7 +1,16 @@
 package com.github.acolote1998.humble_gladiators_2.core.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserModerationService {
+    GeminiService geminiService;
+
+    @Autowired
+    public UserModerationService(GeminiService geminiService) {
+        this.geminiService = geminiService;
+    }
 }
